702bb6d6d8816d65fd466cb82626f9b42b096ecd
Ghanishth Ahuja <ahujaghanishth@gmail.com>
2026-04-16 18:25:28 +0530

    changed to fly

--- SIGN-BRIDGE-AI/main.py
+++ SIGN-BRIDGE-AI/main.py
@@ -592,3 +592,4 @@ app = initialize_app()
 if __name__ == "__main__":
     print(f"[Server] Starting development server on http://0.0.0.0:5000")
     socketio.run(app, host='0.0.0.0', port=5000, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
+#done
