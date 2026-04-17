848b6419645bf1e2fc2351d933eab19c568ecb93
Zeeshan <zk45681@gmail.com>
2026-04-17 09:56:24 +0530

    url

--- web/src/constants.js
+++ web/src/constants.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'https://director-addresses-working-kinase.trycloudflare.com/';
+export const API_BASE_URL = 'https://director-addresses-working-kinase.trycloudflare.com';
